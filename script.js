d713d6b094353c83e98b387279513831662bb27b
Jenya <pirozheka@gmail.com>
2022-01-18 19:05:45 +0500

    adaptive for WP

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
+const div = document.querySelector('.canv_container');
+let width = div.clientWidth;
+let height = div.clientHeight;
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera( 30, width / height, 0.1, 1000 );
 //scene constructor
 
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+renderer.setSize( width, height );
+div.appendChild( renderer.domElement );
 //renderer constructor
 
 let spotlight = new THREE.SpotLight(0x6FA8DC);
@@ -12,7 +15,7 @@ spotlight.position.set(40,1,90);
 scene.add(spotlight);
 //creating spotlight color: 0x6FA8DC
 
-const texture = new THREE.TextureLoader().load( "textures/Screenshot_1.png" );
+const texture = new THREE.TextureLoader().load( "https://aurora-starship.space/wp-content/uploads/2022/01/Screenshot_1.png" );
 
 
 let geometry = new THREE.SphereGeometry( 15, 51, 26 );
@@ -24,7 +27,7 @@ camera.position.z = 65;
 
 function animate() {
 				requestAnimationFrame( animate );
-				sphere.rotation.y += 0.01;
+				sphere.rotation.y += 0.005;
 				renderer.render( scene, camera );
  };
 

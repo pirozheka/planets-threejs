f6a8cf0aba69f68cde93863f897e67cd6ffc7626
Jenya <pirozheka@gmail.com>
2022-01-18 19:28:33 +0500

    background textures (not working)

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const div = document.querySelector('.canv_container');
 let width = div.clientWidth;
 let height = div.clientHeight;
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 30, width / height, 0.1, 1000 );
+const backTexture = new THREE.TextureLoader().load( "/textures/sp.jpg" );
+const scene = new THREE.Scene( { environment: backTexture } );
+const camera = new THREE.PerspectiveCamera( 45, width / height, 0.1, 100 );
 //scene constructor
 
 const renderer = new THREE.WebGLRenderer();
@@ -11,11 +12,11 @@ div.appendChild( renderer.domElement );
 //renderer constructor
 
 let spotlight = new THREE.SpotLight(0x6FA8DC);
-spotlight.position.set(40,1,90);
+spotlight.position.set(80,1,100);
 scene.add(spotlight);
 //creating spotlight color: 0x6FA8DC
 
-const texture = new THREE.TextureLoader().load( "https://aurora-starship.space/wp-content/uploads/2022/01/Screenshot_1.png" );
+const texture = new THREE.TextureLoader().load( "/textures/Screenshot_1.png" );
 
 
 let geometry = new THREE.SphereGeometry( 15, 51, 26 );
@@ -23,7 +24,7 @@ const material = new THREE.MeshLambertMaterial( { map: texture } );
 const sphere = new THREE.Mesh (geometry, material);
 scene.add( sphere );
 
-camera.position.z = 65;
+camera.position.z = 55;
 
 function animate() {
 				requestAnimationFrame( animate );
